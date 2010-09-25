f4345e090c09d5ed9bfc8d82a580268ea6624483
Gabriel Mansour <gabrielmansour@gmail.com>
2010-09-25 13:08:06 -0400

    Add Event Calendar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,14 @@
 class EventsController < ApplicationController
   make_resourceful do
     actions :all
+    
+    before :index do
+      @month = (params[:month] || Time.zone.now.month).to_i
+      @year = (params[:year] || Time.zone.now.year).to_i
+
+      @shown_month = Date.civil(@year, @month)
+
+      @event_strips = Event.event_strips_for_month(@shown_month)
+    end
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,3 @@
 class Event < ActiveRecord::Base
+  has_event_calendar :start_at_field  => :dtstart, :end_at_field => :dtend
 end
